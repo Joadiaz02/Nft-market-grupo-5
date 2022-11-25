3f0cdf0efda891f5be2dcb8d7ece103c951ca597
Ezequiel Carrasco <ezequielcarrasco96@gmail.com>
2022-11-25 01:09:13 -0300

    Registro Usuarios: Validación campos (excepto imagen) + creación de usuarios json

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
 const path=require('path');
+const bcrypt = require('bcryptjs');
+
+const { validationResult } = require('express-validator');
+
+const usuariosFilePath = path.join(__dirname, '../data/users.json');
+let usuariosJson = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8'));
 
 const registroController={
     registro: (req, res) =>{
@@ -6,7 +13,43 @@ const registroController={
     },
     login: (req, res) =>{
         res.render('../views/usuarios/login');
+    },
+    /*processRegister: (req, res) =>
+    {
+        const resultValidation = validationReuslt(req);
+        if(resultValidation.errors.length > 0){
+            return res.render('../views/usuarios/registro', {
+                errors: resultValidation.mapped()})
+            }
+        }
+    ,*/
+    store: (req, res) => { 
+        const resultValidation = validationResult(req);
+        if(resultValidation.errors.length > 0){
+            return res.render('../views/usuarios/registro', {
+                errors: resultValidation.mapped()})
+                /*return res.send(req.body)*/
+                /*return res.send(resultValidation.mapped())*/
+            }else{
+
+        let nuevoUsuario = {
+        id: usuariosJson[usuariosJson.length - 1].id + 1,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        usuario: req.body.usuario,
+        mail: req.body.mail,
+        nacimiento: req.body.nacimiento,
+        pais_residencia: req.body.pais_residencia,
+        intereses: req.body.intereses,
+        foto: req.file.filename,
+        contraseña: bcrypt.hashSync(req.body.contrasena, 10)
     }
+    usuariosJson.push(nuevoUsuario);
+    fs.writeFileSync(usuariosFilePath, JSON.stringify(usuariosJson, null, " "));
+    res.redirect('/usuario/registro');}
+
+
+}
 }
 
 module.exports = registroController;
--- src/routers/rutaUsuario.js
+++ src/routers/rutaUsuario.js
@@ -2,7 +2,29 @@ let express = require('express');
 let usuarioController = require('../controllers/usuarioController');
 let router = express.Router();
 
+const uploadFile = require('../middlewares/multerMiddleware');
+
+//validation
+const { body } = require('express-validator');
+
+const validations = [
+    body('nombre').notEmpty().withMessage('Tienes que escribir tu nombre'),
+    body('apellido').notEmpty().withMessage('Tienes que escribir tu apellido'),
+    body('usuario').notEmpty().withMessage('Tienes que escribir un usuario'),
+    body('mail').notEmpty().withMessage('Tienes que escribir un correo electrónico'),
+    body('nacimiento').notEmpty().withMessage('Tenes que indicar tu fecha de nacimiento'),
+    body('pais_residencia').notEmpty().withMessage('Tienes elegir un país'),
+    body('contrasena').notEmpty().withMessage('Tienes colocar una contraseña'),
+    body('confirmar_contrasena').notEmpty().withMessage('Tienes que confirmar la contraseña')
+];
+
+
+//Rutas
 router.get('/login', usuarioController.login);
+
+
+//Crear Usuario
 router.get('/registro', usuarioController.registro);
+router.post('/detalle', uploadFile.single("filename"), validations, usuarioController.store);
 
 module.exports = router;
