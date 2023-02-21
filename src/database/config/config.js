b2cdeba6278d46a2e50e51ef6d363cadb8aef427
joadiaz02 <joacodiaz02@hotmail.com>
2023-02-21 01:17:52 -0300

    subiendo banner

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "barquelona2002",
     "database": "nft_market",
     "host": "127.0.0.1",
     "dialect": "mysql"
